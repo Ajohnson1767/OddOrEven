91e0c963e488ac5adf3a96816758b15b27e025d6
Aaron <aaron.johnson35043@gmail.com>
2020-10-27 12:01:17 -0500

    Odd Even Exercise

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -4,9 +4,35 @@ namespace OddOrEven
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool answer = IsOdd(5);
+
+            Console.WriteLine(answer);
+            
         }
+        //Start Here
+        public static bool IsOdd(int num)
+        {
+
+
+            //Determines if a number is odd
+            if (num % 2 == 0)
+            {
+
+                Console.WriteLine("Number is even");
+                return false;
+                
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+                return true;
+                
+            }
+
+        }
+
     }
 }
